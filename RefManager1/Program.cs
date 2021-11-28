c9cba587daa3c728734760e102ca0bdb6a61107f
Александр Иванович Самаркин <alexsamarkinru@gmail.com>
2021-11-29 00:21:54 +0300

    плюс самостоятельная работа

--- RefManager1/Program.cs
+++ RefManager1/Program.cs
@@ -17,6 +17,49 @@ namespace RefManager1
             Source source = new Source();
             Console.WriteLine(source);
             Console.ReadKey();
+
+            /// После добавления свойств к менеджеру и проекту можно их создать и распечатать
+            ///
+            
+            /// Менеджер проектов
+            /// в фигурных скобках - конструкторы полей
+            RefManager manager = new RefManager()
+            {
+                Id = 1, BaseFolder = "Мои проекты",
+                MainTheme = "Медицина катастроф",
+                User = "Student",
+                Projects = new List<RefProject>() // не обязательно, так как список будет создан
+            };
+
+            /// Проект
+            RefProject project = new RefProject()
+            {
+                Author = new Author()
+                {
+                    Name = "Иван",
+                    MiddleName = "Иванович",
+                    LastName = "Иванов"
+                },
+                ProjectName = "Катастрофы в сельском хозяйстве",
+                Description = "Как вывести кротов с дачного участка"
+            };
+
+            /// Добавляем проект в менеджер
+            manager.Projects.Add(project);
+
+            /// САМОСТОЯТЕЛЬНО
+            /// 1 установите точку отладки на строку manager.Projects.Add(project);
+            /// просмотрите содержимое manager
+            /// 2 добавьте к классу проекта список ссылок (List<RefBase> RefList {get; set;} = new ...)
+            /// пользуйтесь prop <Tab>
+            /// 3 создайте ссылку (содержимое - на ваше усмотрение) RefBase refBase = new RefBase(){}
+            /// 4 добавьте ссылку к списку ссылок project
+            /// project.RefList.Add(refBase)
+            /// 5 подумайте, будет ли эта ссылка видна в менеджере
+            /// проверьте в отладчике ваше предположение
+
+
+
         }
     }
 }
